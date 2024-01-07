bcd9e70ef9f913d0e021d0eb2654a05bb0809772
Ishmael Aqsar <isi_786@hotmail.com>
2024-01-07 18:17:56 +0100

    fix emacs theme/font

--- config/.emacs.d/init.el
+++ config/.emacs.d/init.el
@@ -1,10 +1,22 @@
-(setq-default electric-indent-inhibit t) ; dont indent previous line on <RET>
+; theme
+(add-to-list 'default-frame-alist '(foreground-color . "black"))
+(add-to-list 'default-frame-alist '(background-color . "light-yellow"))
 
-(load-theme 'leuven t)
+; font
+(defun font-available-p (font-name)
+  (find-font (font-spec :name font-name)))
+(cond
+ ((font-available-p "Iosevka Term")
+  (set-frame-font "Iosevka Term-10"))
+ ((font-available-p "DejaVu Sans Mono")
+  (set-frame-font "DejaVu Sans Mono-11"))
+ ((font-available-p "Unifont")
+  (set-frame-font "Unifont-11")))
+
 (global-font-lock-mode 0) ; turn off syntax highlighting
-
 (add-hook 'prog-mode-hook 'display-line-numbers-mode)
 
+(setq-default electric-indent-inhibit t) ; dont indent previous line on <RET>
 (setq-default indent-tabs-mode nil) ; use spaces instead of tabs
 (setq c-basic-offset 4)
 (setq c-basic-indent 4)
