601a23e5dd79a743c8b453173b261c6905b0dd5e
Ishmael Aqsar <isi_786@hotmail.com>
2024-01-18 06:28:35 +0100

    fix

--- config/.emacs.d/init.el
+++ config/.emacs.d/init.el
@@ -1,28 +1,3 @@
-; theme
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
-(if (daemonp)
-    (add-hook 'after-make-frame-functions
-        (lambda (frame)
-            (select-frame frame)
-            (load-theme 'nord t)))
-    (load-theme 'nord t))
-
-; font
-(defun font-available-p (font-name)
-  (find-font (font-spec :name font-name)))
-(defun try-set-font ()
-  (cond
-   ((font-available-p "Iosevka Term")
-    (add-to-list 'default-frame-alist '(font . "Iosevka Term-10")))
-   ((font-available-p "DejaVu Sans Mono")
-    (add-to-list 'default-frame-alist '(font . "DejaVu Sans Mono-11")))))
-(if (daemonp)
-    (add-hook 'after-make-frame-functions
-              (lambda (frame)
-                (select-frame frame)
-                (try-set-font)))
-  (try-set-font))
-
 (setq font-lock-maximum-decoration 1)
 (add-hook 'prog-mode-hook 'display-line-numbers-mode)
 
